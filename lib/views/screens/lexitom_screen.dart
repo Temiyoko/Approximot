9e1c84ac3b73f1a27fa93362047b6867b8791a85
justfreezo <pisraelmartins@gmail.com>
2024-11-30 18:01:46 +0100

    New transition fix

--- lib/views/screens/lexitom_screen.dart
+++ lib/views/screens/lexitom_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../../utils/page_transitions.dart';
 import 'home_screen.dart';
 import 'wikitom_screen.dart';
 import 'settings_screen.dart';
