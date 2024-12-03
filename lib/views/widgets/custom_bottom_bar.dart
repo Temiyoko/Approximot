55f2950a905b3cf80e7dfafd716a3347d32bec3d
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2024-12-03 16:49:09 +0100

    Bottom bar edit + new logos

--- lib/views/widgets/custom_bottom_bar.dart
+++ lib/views/widgets/custom_bottom_bar.dart
@@ -17,6 +17,13 @@ class CustomBottomBar extends StatelessWidget {
       height: 80,
       decoration: BoxDecoration(
         color: const Color(0xFF303030),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
         border: Border(
           top: BorderSide(
             color: Colors.white.withOpacity(0.1),
@@ -27,20 +34,20 @@ class CustomBottomBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildIcon(0, Icons.mode_edit_outline),
-          _buildWikiIcon(1),
-          _buildIcon(2, Icons.settings),
+          _buildNavItem(0, 'assets/images/lexitom_logo.svg', 'Lexitom'),
+          _buildNavItem(1, 'assets/images/wikitom_logo.svg', 'Wikitom'),
+          _buildNavItem(2, 'assets/images/settings_logo.svg', 'Paramètres'),
         ],
       ),
     );
   }
 
-  Widget _buildIcon(int index, IconData icon) {
+  Widget _buildNavItem(int index, String svgPath, String label) {
     final bool isSelected = currentIndex == index;
     return Tooltip(
       preferBelow: false,
       verticalOffset: 50,
-      message: _getTooltipText(index),
+      message: label,
       textStyle: const TextStyle(
         fontSize: 14,
         fontFamily: 'Poppins',
@@ -54,93 +61,58 @@ class CustomBottomBar extends StatelessWidget {
           width: 0.5,
         ),
       ),
-      child: AnimatedScale(
-        scale: isSelected ? 1.2 : 1.0,
-        duration: const Duration(milliseconds: 200),
+      child: InkWell(
+        onTap: () => onTap(index),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
         child: Container(
-          decoration: isSelected ? BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white.withOpacity(0.3),
-                blurRadius: 15,
-                spreadRadius: 1,
+          width: 60,
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimatedScale(
+                scale: isSelected ? 1.2 : 1.0,
+                duration: const Duration(milliseconds: 200),
+                child: Container(
+                  decoration: isSelected ? BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.3),
+                        blurRadius: 15,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ) : null,
+                  child: SvgPicture.asset(
+                    svgPath,
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                      isSelected ? Colors.white : Colors.white30,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+              ),
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: isSelected ? 1.0 : 0.0,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  margin: const EdgeInsets.only(top: 4),
+                  width: 4,
+                  height: 4,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
               ),
             ],
-          ) : null,
-          child: IconButton(
-            icon: Icon(
-              icon,
-              size: 24,
-            ),
-            color: isSelected ? Colors.white : Colors.white30,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            onPressed: () => onTap(index),
           ),
         ),
       ),
     );
-  }
-
-  Widget _buildWikiIcon(int index) {
-    final bool isSelected = currentIndex == index;
-    return Tooltip(
-      preferBelow: false,
-      verticalOffset: 50,
-      message: _getTooltipText(index),
-      textStyle: const TextStyle(
-        fontSize: 14,
-        fontFamily: 'Poppins',
-        color: Colors.white,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFF303030),
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: Colors.white.withOpacity(0.1),
-          width: 0.5,
-        ),
-      ),
-      child: AnimatedScale(
-        scale: isSelected ? 1.2 : 1.0,
-        duration: const Duration(milliseconds: 200),
-        child: Container(
-          decoration: isSelected ? BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white.withOpacity(0.3),
-                blurRadius: 15,
-                spreadRadius: 1,
-              ),
-            ],
-          ) : null,
-          child: IconButton(
-            icon: SvgPicture.asset(
-              'assets/images/wikitom_logo.svg',
-              width: 24,
-              height: 24,
-              colorFilter: ColorFilter.mode(
-                isSelected ? Colors.white : Colors.white30,
-                BlendMode.srcIn,
-              ),
-            ),
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            onPressed: () => onTap(index),
-          ),
-        ),
-      ),
-    );
-  }
-
-  String _getTooltipText(int index) {
-    return switch (index) {
-      0 => 'Lexitom',
-      1 => 'Wikitom',
-      2 => 'Paramètres',
-      _ => '',
-    };
   }
 }
